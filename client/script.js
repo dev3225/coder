dbce129dabf41fd1363388dc5b744f731bb97383
dev3225 <kudarshan@gmail.com>
2023-03-04 23:16:57 +0100

    first commit

---
+++ client/script.js
@@ -0,0 +1,45 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');  //used to target specific elements in html as we are using vanilla js
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+/*  This function is to show visualization in the answer box '°°°', 
+    while the API collects data which needs to be shown to the user.  */
+function loader(element) {
+  element.textContent = '';
+  loadInterval = setInterval(() => {
+    element.textContent += '°';
+    if (element.textContent === '°°°')
+      element.textContent = '';
+  }, 300)
+}
+
+/*  This function helps us to show the answers in a humanly possible manner. 
+    Similar to how a we take time to convey our thoughts. */
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+/*  This function helps us to generate a unique ID from 
+    current time and a random number generated from JS library functions  */
+function generateUniqueId() {
+  const timeStamp = Date.now();
+  const randomNum = Math.random();
+  const hexadecstr = randomNum.toString(16);
+
+  return `id-${timeStamp}-${hexadecstr}`;       //returning the Unique Id as a string.
+}
+
+

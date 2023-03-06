da553e25a23e16ad7fd89a4fea525189aca2e382
Dev Darshan Kasthuri Umashankar <kudarshan@gmail.com>
2023-03-06 13:00:59 +0100

    Enter key functionality bug fix

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
-  if (e.keycode === 13) {
+  if (e.keyCode === 13) {
     handleSubmit(e);
   }
 })

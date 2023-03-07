dfd666e3628f3a7480ac8e0248f905290dff4f07
dev3225 <kudarshan@gmail.com>
2023-03-07 14:38:57 +0100

    update the server URL

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //Fetchc data from server - bot's response
-  const response = await fetch('http://localhost:1080',{
+  const response = await fetch('https://coder-crdd.onrender.com',{
     method: 'POST',
     headers:{
       'Content-Type': 'application/json'
